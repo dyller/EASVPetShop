76a1d192a68bdc6ae8ef2308948b5b8f5d583411
liverpoljer <liverpoljer@live.dk>
2018-11-16 11:12:43 +0100

    sdf

--- PetShopAPI/Startup.cs
+++ PetShopAPI/Startup.cs
@@ -118,6 +118,7 @@ namespace PetShopAPI
                 {
                     var ctx = scope.ServiceProvider.GetService<PetShopAppContext>();
                     ctx.Database.EnsureCreated();
+                    DBInitilizer.SeedDB(ctx);
                 }
                 app.UseHsts();
             }
